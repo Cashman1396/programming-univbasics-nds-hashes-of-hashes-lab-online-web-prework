2ecedd908fe8def50bb5009b140f888ee3a3fd31
Chris Ashman <chrisashman96@gmail.com>
2020-01-20 23:49:58 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -31,7 +31,7 @@
 # }
 
 def naming_system
-  {label: "Kingdom",
+label: "Kingdom",
     sub_category: {label: "Phylum",
       sub_category: {label: "Class",
         sub_category: {label:"Order",
@@ -45,5 +45,5 @@ def naming_system
     }
   }
 }
-}
+
 end
