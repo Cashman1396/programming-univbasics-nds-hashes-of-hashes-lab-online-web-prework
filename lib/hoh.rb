759e866df4e469d600b4c63e83c0c0cc8e30fb14
Chris Ashman <chrisashman96@gmail.com>
2020-01-20 23:44:12 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -38,7 +38,9 @@ def naming_system
    sub_category: {label: "Order", 
      sub_category: {label: "Genus", 
      sub_category: {label: "Species", 
-       sub_category: {label: "nil"}
+       sub_category: {label: "nil"
+         
+          }
         }
       }
      }
